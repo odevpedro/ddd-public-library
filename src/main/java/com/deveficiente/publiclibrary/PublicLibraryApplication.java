6833933850250e03e675a0cda5f0ebb4745f6e01
odevpedro <pedrosschmidt2@gmail.com>
2022-06-22 00:23:25 -0300

    Iniciando o projeto

---
+++ src/main/java/com/deveficiente/publiclibrary/PublicLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.deveficiente.publiclibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicLibraryApplication.class, args);
+	}
+
+}
